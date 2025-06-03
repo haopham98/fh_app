de36f4cb49583aa0dd765487aa2ad3711fd50437
Pham Hoang Hao <phhao6798@gmail.com>
2025-06-03 16:02:01 +0700

    feat(accounts): create register api

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include
 from . import views
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+
 urlpatterns = [
     path('users/', views.user_list),
-    path('users/<int:pk>/', views.user_detail)  # Include user profile URLs
+    path('users/<int:pk>/', views.user_detail),  # Include user profile URLs
+    path('register/', views.register)
 ]
